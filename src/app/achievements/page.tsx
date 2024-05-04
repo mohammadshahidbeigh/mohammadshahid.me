75725494304ff28b3429a946b0fd0d60623aad80
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-05-04 20:17:39 +0530

    chore: Remove unused code in achievements page

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -1,14 +1,14 @@
-import { Metadata } from "next";
-import AchievementsPage from "@/components/AchievementsPage";
+// import { Metadata } from "next";
+// import AchievementsPage from "@/components/AchievementsPage";
 
-export const metadata: Metadata = {
-  title: 'Achievements'
-}
+// export const metadata: Metadata = {
+//   title: 'Achievements'
+// }
 
-export default function Achievements(): JSX.Element {
-  return (
-    <main className=" min-h-screen p-0 min-[950px]:max-[1450px]:ml-20 max-[500px]:text-sm ">
-      <AchievementsPage />
-    </main>
-  )
-}
+// export default function Achievements(): JSX.Element {
+//   return (
+//     <main className=" min-h-screen p-0 min-[950px]:max-[1450px]:ml-20 max-[500px]:text-sm ">
+//       <AchievementsPage />
+//     </main>
+//   )
+// }
