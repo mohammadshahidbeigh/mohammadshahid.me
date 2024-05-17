1ecc8d910f8ead9fa7a762186f05fc85d76e346d
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-05-17 19:19:10 +0530

    chore: Add social media icons to ContactPage component

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -1,5 +1,19 @@
-import { SiGithub, SiLinkedin, SiGmail } from "react-icons/si";
-import { EMAIL, GITHUB_URL, LINKEDIN_URL } from "@/constants";
+import {
+  SiGithub,
+  SiLinkedin,
+  SiGmail,
+  SiInstagram,
+  SiTwitter,
+  SiStackoverflow,
+} from "react-icons/si";
+import {
+  EMAIL,
+  GITHUB_URL,
+  LINKEDIN_URL,
+  INSTAGRAM_URL,
+  X_URL,
+  STACKOVERFLOW_URL,
+} from "@/constants";
 
 export default function ContactPage(): JSX.Element {
   return (
@@ -9,26 +23,46 @@ export default function ContactPage(): JSX.Element {
     >
       <div className="container mx-auto animate-fade-in">
         <h2 className="title-ui mb-8">Get in touch</h2>
-        <div className="my-2 flex  justify-center space-x-4 mb-6 rounded-md w-min mx-auto border border-[#b4b4b4] dark:border-[#1e1e1e] outline-1 p-2">
-          <a href={EMAIL} className=" project-icons hover:bg-blue-500 ">
-            {" "}
-            <SiGmail className="text-3xl max-[500px]:text-xl" />{" "}
+        <div className="my-2 flex justify-center space-x-4 mb-6 rounded-md w-min mx-auto border border-[#b4b4b4] dark:border-[#1e1e1e] outline-1 p-2">
+          <a href={EMAIL} className="project-icons hover:bg-blue-500">
+            <SiGmail className="text-3xl max-[500px]:text-xl" />
           </a>
           <a
             href={LINKEDIN_URL}
             target="_blank"
-            className=" project-icons hover:bg-blue-500"
+            className="project-icons hover:bg-blue-500"
           >
-            {" "}
-            <SiLinkedin className="text-3xl max-[500px]:text-xl" />{" "}
+            <SiLinkedin className="text-3xl max-[500px]:text-xl" />
           </a>
           <a
             href={GITHUB_URL}
             target="_blank"
-            className=" project-icons hover:bg-blue-500"
+            className="project-icons hover:bg-blue-500"
           >
-            {" "}
-            <SiGithub className="text-3xl max-[500px]:text-xl" />{" "}
+            <SiGithub className="text-3xl max-[500px]:text-xl" />
+          </a>
+        </div>
+        <div className="my-2 flex justify-center space-x-4 mb-6 rounded-md w-min mx-auto border border-[#b4b4b4] dark:border-[#1e1e1e] outline-1 p-2">
+          <a
+            href={INSTAGRAM_URL}
+            target="_blank"
+            className="project-icons hover:bg-red-500"
+          >
+            <SiInstagram className="text-3xl max-[500px]:text-xl" />
+          </a>
+          <a
+            href={X_URL}
+            target="_blank"
+            className="project-icons hover:bg-blue-500"
+          >
+            <SiTwitter className="text-3xl max-[500px]:text-xl" />
+          </a>
+          <a
+            href={STACKOVERFLOW_URL}
+            target="_blank"
+            className="project-icons hover:bg-orange-500"
+          >
+            <SiStackoverflow className="text-3xl max-[500px]:text-xl" />
           </a>
         </div>
       </div>
--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -24,13 +24,12 @@ export const KNOWIT_ALL_URL: string =
 export const AUTOMOBILES_PRICE_URL: string =
   "https://github.com/mohammadshahidbeigh/automobiles-price-esstimation";
 
-// export const Achievements: string[] = [
-//   "Google Venkat Scholar 2022 - Selected as one of the 14 recipients of the Google Venkat Scholarship among 20,000 applicants across India.",
-//   "5-Star Golden badge in C++ and SQL on HackerRank.",
-//   "Secured Rank-1 in State Level Talent Search Examination – 2015.",
-//   "Foundation for Excellence Scholar, 2019 - 2023",
-//   "Kashmir Education Initiative Scholar, 2014 - 2020.",
-// ];
+export const INSTAGRAM_URL: string =
+  "https://www.instagram.com/m0hammadshahid_?igsh=dzVjMWozN3VheGQ2";
+export const X_URL: string =
+  "https://x.com/m0hammad_shahid?t=yHwKObUdCDXJX2NYeSJ4eA&s=08";
+export const STACKOVERFLOW_URL: string =
+  "https://stackoverflow.com/users/21755863/mohammad-shahid-beigh?tab=profile";
 
 export const NavList = [
   {
@@ -54,3 +53,26 @@ export const NavList = [
     href: "/contact",
   },
 ];
+
+// Add Socials section
+export const Socials = `
+### Socials
+
+<div id="badges">
+  <a href="https://www.linkedin.com/in/mohammad-shahid-beigh/">
+    <img src="https://img.shields.io/badge/LinkedIn-blue?style=for-the-badge&logo=linkedin&logoColor=white" alt="LinkedIn Badge"/>
+  </a>
+  <a href="https://x.com/m0hammad_shahid?t=yHwKObUdCDXJX2NYeSJ4eA&s=08">
+    <img src="https://img.shields.io/badge/Twitter-blue?style=for-the-badge&logo=twitter&logoColor=white" alt="Twitter Badge"/>
+  </a>
+  <a href="https://www.instagram.com/m0hammadshahid_?igsh=dzVjMWozN3VheGQ2">
+    <img src="https://img.shields.io/badge/Instagram-darkred?style=for-the-badge&logo=instagram&logoColor=white" alt="Instagram Badge"/>
+  </a>
+  <a href="mailto:mohammadshahidbeigh@gmail.com">
+    <img src="https://img.shields.io/badge/Gmail-FF0000?style=for-the-badge&logo=gmail&logoColor=white" alt="Gmail Badge"/>
+  </a>
+  <a href="https://stackoverflow.com/users/21755863/mohammad-shahid-beigh?tab=profile">
+    <img src="https://img.shields.io/badge/Stack Overflow-FE7A16?style=for-the-badge&logo=stack-overflow&logoColor=white" alt="Stack Overflow Badge"/>
+  </a>
+</div>
+`;
