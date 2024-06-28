3c25b062ce166988addf3915baa4915add2d7b16
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-06-28 17:51:20 +0530

    "Updated TestimonialsPage component with new layout and styling"

--- src/components/TestimonialsPage.tsx
+++ src/components/TestimonialsPage.tsx
@@ -13,29 +13,38 @@ const testimonials = [
 
 export default function TestimonialsPage(): JSX.Element {
   return (
-    <div className="flex flex-col items-center p-4 sm:p-6">
-      {testimonials.map((testimonial) => (
-        <div
-          key={testimonial.id}
-          className="bg-gradient-to-r from-purple-500 to-cyan-500 hover:from-purple-700 hover:to-cyan-700 shadow-md rounded-lg p-2 sm:p-4 mb-6 sm:mb-8 w-full max-w-3xl"
-        >
-          <div className="flex flex-col sm:flex-row items-center mb-4">
-            <Image
-              src={testimonial.image}
-              alt={testimonial.author}
-              width={100}
-              height={100}
-              className="rounded-full mb-4 sm:mb-0 sm:mr-4"
-            />
-            <div className="text-center sm:text-left">
-              <p className="text-lg sm:text-xl font-bold">{testimonial.text}</p>
-              <p className="text-black  font-semibold mt-2">
-                — {testimonial.author}, {testimonial.position}
-              </p>
+    <div className="main-div-ui bg-[#030018]">
+      <div className="animate-fade-in max-w-screen-xl mx-auto p-4 md:p-8">
+        <h2 className="title-ui text-3xl font-bold text-white mb-8">
+          Testimonials
+        </h2>
+        <div className="flex flex-col items-center p-4 sm:p-6">
+          {testimonials.map((testimonial) => (
+            <div
+              key={testimonial.id}
+              className="bg-gradient-to-r from-purple-500 to-cyan-500 hover:from-purple-700 hover:to-cyan-700 shadow-md rounded-lg p-2 sm:p-4 mb-6 sm:mb-8 w-full max-w-3xl"
+            >
+              <div className="flex flex-col sm:flex-row items-center mb-4">
+                <Image
+                  src={testimonial.image}
+                  alt={testimonial.author}
+                  width={100}
+                  height={100}
+                  className="rounded-full mb-4 sm:mb-0 sm:mr-4"
+                />
+                <div className="text-center sm:text-left">
+                  <p className="text-lg sm:text-xl font-bold">
+                    {testimonial.text}
+                  </p>
+                  <p className="text-black font-semibold mt-2">
+                    — {testimonial.author}, {testimonial.position}
+                  </p>
+                </div>
+              </div>
             </div>
-          </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
