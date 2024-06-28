dfc8c2452abb3698eaf7f9484942f6c31db93af4
Mohammad Shahid Beigh <85876937+MohammadShahidBeigh@users.noreply.github.com>
2024-06-28 17:57:24 +0530

    "Update font size of 'Testimonials' title on medium screens and above"

--- src/components/TestimonialsPage.tsx
+++ src/components/TestimonialsPage.tsx
@@ -15,7 +15,7 @@ export default function TestimonialsPage(): JSX.Element {
   return (
     <div className="main-div-ui bg-[#030018]">
       <div className="animate-fade-in max-w-screen-xl mx-auto p-4 md:p-8">
-        <h2 className="title-ui text-3xl font-bold text-white mb-8">
+        <h2 className="title-ui md:text-2xl font-bold text-white mb-8">
           Testimonials
         </h2>
         <div className="flex flex-col items-center p-4 sm:p-6">
